9d93ff06bf7e3bf24e62b7e82b0709d28b90b82c
zeze.li <lizeze.boy@gmail.com>
2020-12-02 16:24:38 +0800

    增加全局拦截器

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import driverRouter from "./driver/router/driverRouter";
 import common from "./common";
+
 const app = express();
 let bodyParser = require('body-parser')
 const port = 3000
@@ -16,12 +17,14 @@ app.use((req, res, next) => {
     next();
 });
 app.use((req, res, next) => {
+
     const accessToken = req.get("accessToken")
     if (!accessToken) {
         res.status(403).send(common.unifyResponse("未登录", 403))
     } else {
         next()
     }
+    next()
 })
 app.use('/api/driver', driverRouter)
 app.listen(port, () =>
